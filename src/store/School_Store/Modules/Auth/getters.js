a679990f89b88166b7351cfe75fae0716b00530c
Magdi Khaled <86855870+Magdi-khaled@users.noreply.github.com>
2024-04-28 23:50:49 +0300

    -unwanted

--- src/store/School_Store/Modules/Auth/getters.js
+++
@@ -1,9 +0,0 @@
-export const getters = {
-    getToken2: (state) => state.token,
-    GetUser2: (state) => state.user,
-    GetStudents: (state) => state.studs,
-    GetRequests: (state) => state.requests,
-    GetEvents: (state) => state.events,
-    GetTeachers: (state) => state.teachers,
-    GetControllers: (state) => state.controllers,
-};
